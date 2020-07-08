a994c6f5791a323ca78962c7589c0b7bbf5aaeba
laisne.gautier <laisne.gautier@gmail.com>
2020-07-08 19:21:06 +0200

    New form and validations

---
+++ WithTeachers/Data/ViewModels/ClassStudents.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WithTeachers.Data.ViewModels
+{
+    public class ClassStudents
+    {
+        [ValidateComplexType]
+        public Class Class { get; set; } = new Class();
+
+        [ValidateComplexType]
+        public List<Student> Students { get; set; } = new List<Student>() { new Student() };
+    }
+}
