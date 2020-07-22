388324b03efd704b0446513ae907c077edc34de1
laisne.gautier <laisne.gautier@gmail.com>
2020-07-22 13:36:31 +0200

    editing done

--- WithTeachers/Data/CourseDocumentService.cs
+++ WithTeachers/Data/CourseDocumentService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 namespace WithTeachers.Data
@@ -11,16 +12,26 @@ namespace WithTeachers.Data
         public CourseDocumentService(ApplicationDbContext context)
             => Context = context;
 
-        public async Task CreateAsync(CourseDocument courseDocument)
+        public async Task CreateAsync(CourseDocument document)
         {
-            Context.CourseDocuments.Add(courseDocument);
+            Context.CourseDocuments.Add(document);
             await Context.SaveChangesAsync();
         }
 
-        public async Task UpdateAsync(CourseDocument courseDocument)
+        public async Task UpdateAsync(CourseDocument document)
         {
-            Context.CourseDocuments.Update(courseDocument);
+            Context.CourseDocuments.Update(document);
             await Context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(CourseDocument document)
+        {
+            CourseDocument courseExists = await Context.CourseDocuments.SingleOrDefaultAsync(x => x.CourseDocumentId == document.CourseDocumentId);
+            if (courseExists != null)
+            {
+                Context.CourseDocuments.Remove(document);
+                await Context.SaveChangesAsync();
+            }
         }
     }
 }
