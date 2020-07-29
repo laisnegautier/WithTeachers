90d2c7b709e7f6c25cf5beafafaf4b4ce805884a
laisne.gautier <laisne.gautier@gmail.com>
2020-07-29 18:08:18 +0200

    Group managing with SignalR

--- OnlineEducation/OnlineEducation/Hubs/WebRTCHub.cs
+++ OnlineEducation/OnlineEducation/Hubs/WebRTCHub.cs
@@ -1,25 +1,20 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace OnlineEducation.Hubs
 {
-    //Using a strongly typed Hub<T> disables the ability to use SendAsync.
-    //Here "Clients.All.name_of_methode(param1, param2, ...)" is the same as "Clients.All.SendAsync("name_of_method", param1, param2, ...)"
-    //More infos: https://docs.microsoft.com/en-us/aspnet/core/signalr/hubs?view=aspnetcore-3.1
-    public interface IWebRTCHub
+    public class WebRTCHub : Hub
     {
-        Task TestCallerClient(string message);
-    }
-
-    public class WebRTCHub : Hub<IWebRTCHub>
-    {
-        public async Task TestCaller()
+        public async Task AddToGroup(string groupName)
         {
-            string ok = "ok CALLER";
-            await Clients.Caller.TestCallerClient(ok);
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Group(groupName).SendAsync("UpdateGroupState", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Group(groupName).SendAsync("UpdateGroupState", $"{Context.ConnectionId} has left the group {groupName}.");
         }
     }
 }
