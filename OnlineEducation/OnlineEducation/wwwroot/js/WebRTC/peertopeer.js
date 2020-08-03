e6502293c561dc54e26fa8a01b6a0ed796388142
laisne.gautier <laisne.gautier@gmail.com>
2020-08-03 08:58:58 +0200

    Stringify JSON value to get RTC offer in .NET

---
+++ OnlineEducation/OnlineEducation/wwwroot/js/WebRTC/peertopeer.js
@@ -0,0 +1,37 @@
+﻿'use strict';
+
+//0.
+window.webRTC_init = () => {
+    const makeCallButton = document.querySelector('#makeCallButton');
+
+    makeCallButton.onclick = makeCall;
+
+    //localVideo.addEventListener('loadedmetadata', function () {
+    //    console.log(`Local video videoWidth: ${this.videoWidth}px,  videoHeight: ${this.videoHeight}px`);
+    //});
+
+    //remoteVideo.addEventListener('loadedmetadata', function () {
+    //    console.log(`Remote video videoWidth: ${this.videoWidth}px,  videoHeight: ${this.videoHeight}px`);
+    //});
+};
+
+//1. Initializing RTCPeerConnection
+async function makeCall() {
+    const configuration = { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }] }
+    const peerConnection = new RTCPeerConnection(configuration);
+
+    //signalingChannel.addEventListener('message', async message => {
+    //    if (message.answer) {
+    //        const remoteDesc = new RTCSessionDescription(message.answer);
+    //        await peerConnection.setRemoteDescription(remoteDesc);
+    //    }
+    //});
+
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+
+    //console.log("offer: " + offer);
+    return JSON.stringify(offer);
+
+    //signalingChannel.send({ 'offer': offer });
+}
